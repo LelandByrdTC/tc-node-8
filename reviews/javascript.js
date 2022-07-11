66a62fe7d52a9bb6ee441d6db1b15a03d5fd96dd
Ben <brbryant2639@gmail.com>
2022-07-11 15:13:25 -0500

    UPDATE: node intro

---
+++ reviews/javascript.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("Hello World!");
+  })
+  .listen(8080, () => {
+    console.log("Server is listening...");
+  });
